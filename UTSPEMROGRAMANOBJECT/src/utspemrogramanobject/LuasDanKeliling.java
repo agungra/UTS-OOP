90512037230af123d25d69da643ee47daaca8aef
agungra <74241702+agungra@users.noreply.github.com>
2020-11-10 21:59:40 -0800

    uts oop persegi dan menghitung ipk

---
+++ UTSPEMROGRAMANOBJECT/src/utspemrogramanobject/LuasDanKeliling.java
@@ -0,0 +1,19 @@
+package utspemrogramanobject;
+
+public class LuasDanKeliling {
+
+    public static void main(String[] args) {
+        int sisi;
+        int hasilKeliling;
+        int hasilLuas;
+        
+        sisi = 29;
+        hasilLuas = sisi * sisi;
+        hasilKeliling = 4 * sisi;
+        
+        System.out.println("Hasil Luas Persegi Adalah = " + hasilLuas);
+        System.out.println("Hasil Keliling Persegi Adalah = " + hasilKeliling);
+    }
+    
+}
+
